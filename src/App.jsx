7d814f076fc4108301e781dd41685b73c96dbd9b
Stephen Behnfeldt <stephen@behnfeldt.name>
2024-06-07 13:09:35 -0400

    404 Not Found page.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} fr
 import MainLayout from "./layouts/MainLayout.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import JobsPage from "./pages/JobsPage.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
 
 const App = () => {
     const router = createBrowserRouter(createRoutesFromElements(
         <Route path='/' element={<MainLayout/>}>
             <Route index element={<HomePage/>}/>
             <Route path='/jobs' element={<JobsPage/>}/>
+            <Route path='*' element={<NotFoundPage/>}/>
         </Route>
     ));
 
