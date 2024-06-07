1ec2140b29aff6565d3864b4e275df2db4351564
Stephen Behnfeldt <stephen@behnfeldt.name>
2024-06-07 13:01:27 -0400

    Jobs page. Convert <a> to <Link> throughout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
 import MainLayout from "./layouts/MainLayout.jsx";
 import HomePage from "./pages/HomePage.jsx";
+import JobsPage from "./pages/JobsPage.jsx";
 
 const App = () => {
     const router = createBrowserRouter(createRoutesFromElements(
         <Route path='/' element={<MainLayout/>}>
             <Route index element={<HomePage/>}/>
+            <Route path='/jobs' element={<JobsPage/>}/>
         </Route>
     ));
 
--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,11 +1,12 @@
 import {useState} from 'react';
+import {Link} from 'react-router-dom';
 import {FaMapMarker} from 'react-icons/fa';
 
 const JobListing = ({job}) => {
     const [showFullDescription, setShowFullDescription] = useState(false);
 
     let description = job.description;
-    if (! showFullDescription) {
+    if (!showFullDescription) {
         description = description.substring(0, 90) + '...';
     }
     return (
@@ -20,7 +21,8 @@ const JobListing = ({job}) => {
                     {description}
                 </div>
 
-                <button onClick={() => setShowFullDescription((prevState) => !prevState)} className="text-indigo-500 mb-5 hover:text-indigo-600">{showFullDescription ? 'Less' : 'More'}</button>
+                <button onClick={() => setShowFullDescription((prevState) => !prevState)}
+                        className="text-indigo-500 mb-5 hover:text-indigo-600">{showFullDescription ? 'Less' : 'More'}</button>
 
                 <h3 className="text-indigo-500 mb-2">{job.salary} / year</h3>
 
@@ -28,15 +30,15 @@ const JobListing = ({job}) => {
 
                 <div className="flex flex-col lg:flex-row justify-between mb-4">
                     <div className="text-orange-700 mb-3">
-                        <FaMapMarker className="inline text-lg mb-1 mr-1" />
+                        <FaMapMarker className="inline text-lg mb-1 mr-1"/>
                         {job.location}
                     </div>
-                    <a
-                        href={`/job/${job.id}`}
+                    <Link
+                        to={`/job/${job.id}`}
                         className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                     >
                         Read More
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
