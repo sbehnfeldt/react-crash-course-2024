00190b007acbc0d8e07d31ac333d6582b04b1866
Stephen <stephen@behnfeldt.pro>
2024-06-07 08:12:02 -0400

    Allow "props" for Hero component. Provide default values.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-const Hero = () => {
+const Hero = ({title = 'Become a React Dev', subtitle = 'Find the React job that fits your skillset'}) => {
     return (
         <section className="bg-indigo-700 py-20 mb-4">
             <div
@@ -8,10 +8,10 @@ const Hero = () => {
                     <h1
                         className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
                     >
-                        Become a React Dev
+                        {title}
                     </h1>
                     <p className="my-4 text-xl text-white">
-                        Find the React job that fits your skills and needs
+                        {subtitle}
                     </p>
                 </div>
             </div>
