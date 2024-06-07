7cbf1277156b49ef131ae787219fb594a14fc40a
Stephen Behnfeldt <stephen@behnfeldt.name>
2024-06-07 13:57:51 -0400

    Conditional rendering

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,6 +1,11 @@
+import JobListings from "../components/JobListings.jsx";
+
 const JobsPage = () => {
     return (
-        <div>Jobs Page</div>
+        <div className="bg-blue-50 px-4 py-6">
+            <JobListings />
+
+        </div>
     )
 };
 export default JobsPage;
