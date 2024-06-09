b39cb712027eb95104b6a1148020f9f97895bc37
Stephen <stephen@behnfeldt.pro>
2024-06-09 06:05:21 -0400

    Fetch job data for single job page using job ID acquired from path by useParam hook

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,4 +1,35 @@
+import {useState, useEffect} from 'react';
+import {useParams} from "react-router-dom";
+import Spinner from "../components/Spinner.jsx";
+
 const JobPage = () => {
-    return <div>JobPage</div>;
+    const {id}                  = useParams();
+    const [job, setJob]         = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchJob = async () => {
+            const apiUrl = `/api/jobs/${id}`;
+
+            try {
+                const res  = await fetch(apiUrl);
+                const data = await res.json();
+                setJob(data);
+
+            } catch (e) {
+                console.error('Error fetch job data: ', e);
+
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchJob();
+    }, [])
+
+    return loading ? <Spinner/> : (
+        <h1>{job.title}</h1>
+    )
 };
+
+
 export default JobPage;
