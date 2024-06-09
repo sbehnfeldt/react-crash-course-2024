b8b88b7a16aa17a735003bdf37321e7de27f7edf
Stephen <stephen@behnfeldt.pro>
2024-06-09 06:46:21 -0400

    Add the "Add Job" page and route thereto.  Set useState for all the fields in the form

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import MainLayout from "./layouts/MainLayout.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import JobsPage from "./pages/JobsPage.jsx";
 import JobPage2, {jobLoader} from "./pages/JobPage2.jsx";
+import AddJobPage from "./pages/AddJobPage.jsx";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
         <Route path='/' element={<MainLayout/>}>
             <Route index element={<HomePage/>}/>
             <Route path='/jobs' element={<JobsPage/>}/>
+            <Route path='/add-job' element={<AddJobPage/>}/>
             <Route path='/jobs/:id' element={<JobPage2/>} loader={jobLoader}/>
             <Route path='*' element={<NotFoundPage/>}/>
         </Route>
