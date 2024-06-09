42aa2e467f4c42371955297ab2986ffc7b0de5d2
Stephen <stephen@behnfeldt.pro>
2024-06-09 06:53:32 -0400

    Create form submit handler (stub) and build object to submit (eventually) to the API

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -2,14 +2,27 @@ import {useState} from "react";
 
 const AddJobPage = () => {
     const [title, setTitle]                           = useState('');
-    const [type, setType]                             = useState('');
+    const [type, setType]                             = useState('Full-Time');
     const [location, setLocation]                     = useState('');
     const [description, setDescription]               = useState('');
-    const [salary, setSalary]                         = useState('');
+    const [salary, setSalary]                         = useState('Under $50K');
     const [companyName, setCompanyName]               = useState('');
     const [companyDescription, setCompanyDescription] = useState('');
     const [contactEmail, setContactEmail]             = useState('');
     const [contactPhone, setContactPhone]             = useState('');
+
+    const submitForm = (event) => {
+        event.preventDefault();
+        const newJob = {
+            title, type, location, description, salary, company: {
+                name: companyName,
+                description: companyDescription,
+                contactEmail,
+                contactPhone
+            }
+        };
+        console.log(newJob);
+    };
 
     return (
         <section className="bg-indigo-50">
@@ -17,7 +30,7 @@ const AddJobPage = () => {
                 <div
                     className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
                 >
-                    <form>
+                    <form onSubmit={submitForm}>
                         <h2 className="text-3xl text-center font-semibold mb-6">Add Job</h2>
 
                         <div className="mb-4">
